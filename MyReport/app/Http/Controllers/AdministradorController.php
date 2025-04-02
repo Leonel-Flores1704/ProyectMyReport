f11b2badff41202a36dfe961315f7f239709e493
Hector <he1.tc2@gmail.com>
2025-04-02 09:38:56 -0600

    Guardar cambios

--- MyReport/app/Http/Controllers/AdministradorController.php
+++ MyReport/app/Http/Controllers/AdministradorController.php
@@ -1,11 +1,12 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Administrador;
 use Illuminate\Http\Request;
 
 class AdministradorController extends Controller{
     public function index(){
-        return view('administrador');
+        $administradores = Administrador::where('id', auth()->id())->get();
+        return view('administrador', compact('administradores'));
     }
 }
